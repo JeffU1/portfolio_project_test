8d7c47c5665c01af63093a91d3588b52ea21e54b
Cem <sabanulusoy488@gmail.com>
2020-07-07 19:17:10 -0400

    revised

--- app.py
+++ app.py
@@ -3,23 +3,23 @@
 from flask import Flask, request, render_template, flash
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html')
 
 
-@app.route('/', methods=['GET','POST'])
-def convertor_post():
+@app.route('/index', methods=['POST','GET'])
+def index_post():
 
     mappings = {1000: "M", 900: 'CM', 500: "D", 400: 'CD', 100: "C",
               90: 'XC', 50: "L", 40: 'XL', 10: "X", 9: 'IX', 5: "V", 4: 'IV', 1: "I"}
     number = request.form['number'] # 1994 "MCMXCIV"
     result = ""
     if not number.isdigit():
-      return render_template('index.html', name='not_valid')     
+      return render_template('index.html')     
     else: 
         if int(number)<1 or int(number)>3999:
-            return render_template('index.html', number=number)
+            return render_template('index.html')
         elif int(number)>=1 and int(number)<=3999:  
             number=int(number)
             for k,v in mappings.items() : # k =1000
@@ -29,7 +29,11 @@ def convertor_post():
                     # "MCMXCIV"  
     return render_template('result.html', number_decimal=request.form['number'], number_roman=result)
 
+@app.route('/result', methods=['POST'])
+def result_get():
+    return (index_post())
+
 if __name__ == '__main__':
-    app.run(debug=True)
-    app.run('localhost', port=5000, debug=True)
+    #app.run(debug=True)
+    #app.run('localhost', port=5000, debug=True)
     app.run('0.0.0.0', port=80)
